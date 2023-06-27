0317722e59741ef727229261a1eaef4fdf006e2e
sooominnn <lsm980830@gmail.com>
2023-06-27 09:48:01 +0900

    스프링부트 프로젝트 시작 - 데브툴즈 - 롬ㅇ복

---
+++ src/main/java/com/soomin/projectboarddevelop/SoominProjectBoardDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.soomin.projectboarddevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoominProjectBoardDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoominProjectBoardDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soomin/projectboarddevelop/SoominProjectBoardDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soomin.projectboarddevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoominProjectBoardDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
