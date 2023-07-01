125243b990bb68699bf068971ae4cda9faf412d0
sooominnn <lsm980830@gmail.com>
2023-07-01 14:44:57 +0900

    게시글 뷰 테스트 내용 업데이트 기존 테스트를 실패한 채 올리면 'gradle build'가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로, 실패 테스트는 테스트 제외 처리하는 방법으로 정책을 정하였다. 기능 구현중인 유닛 테스트를 모두 @Disabled 처리

---
+++ src/main/java/com/soomin/projectboarddevelop/controller/ArticleController.java
@@ -0,0 +1,19 @@
+package com.soomin.projectboarddevelop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * fileName     : ArticleController
+ * author       : lia
+ * date         : 2023/07/01
+ * description  : 게시글 Controller
+ * ===========================================================
+ * DATE            AUTHOR         NOTE
+ * -----------------------------------------------------------
+ * 2023/07/01       lia          최초 생성
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
---
+++ src/test/java/com/soomin/projectboarddevelop/controller/ArticleControllerTest.java
@@ -0,0 +1,89 @@
+package com.soomin.projectboarddevelop.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * fileName     : ArticleControllerTest
+ * author       : lia
+ * date         : 2023/07/01
+ * description  : ArticleController 테스트
+ * ===========================================================
+ * DATE            AUTHOR         NOTE
+ * -----------------------------------------------------------
+ * 2023/07/01       lia          최초 생성
+ */
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) // 해당 클래스만 불러와서 테스트 진행함
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
+    }
+
+}
